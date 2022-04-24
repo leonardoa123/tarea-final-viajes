abf1df03b86513482efc2041ba796f7df82515aa
Leonardo <leonardoa123@gmail.com>
2022-04-24 01:30:48 -0400

    commit final

---
+++ assets/JS/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#destacados").click(function () {
+        $("#desaparece").toggle("slow");
+    });
+});
+$(document).ready(function () {
+    $("#mensage").click(function () {
+        alert("Gracias por enviar su mensaje")
+    });
+}); 
